d5df3d4a7fec6434e118cf9a6e7044f026fb4fd4
techshin31 <dlsrlrk@gmail.com>
2026-06-09 11:27:27 +0900

    web파일정리,유기견정보,마이페이지수정

--- web/dog/urls.py
+++ web/dog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
-from .views import get_page
 
 app_name = "dog"
 
 urlpatterns = [
-    path("", get_page, name="home"),
+    path("", RedirectView.as_view(pattern_name="dog:search", permanent=False), name="home"),
     path("breeds/", views.search, name="search"),
     path("breeds/<int:pk>/", views.detail, name="detail"),
     path("breeds/<int:pk>/favorite/", views.toggle_favorite, name="toggle_favorite"),
